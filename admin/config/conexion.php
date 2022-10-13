fc74439ef041e7b48e383417db6ebaef446514d4
bj2398 <brajperales@misena.edu.co>
2022-10-13 10:12:57 -0500

    se cambio la conexion

--- admin/config/conexion.php
+++ admin/config/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "bdsitio");
+$conn = new mysqli("bqj8rv2f9dbjrnp1a9q2-mysql.services.clever-cloud.com", "urqpzztkbmlo8u8u", "Mb0KomVYdlkkY5FGjN30", "bqj8rv2f9dbjrnp1a9q2");
   
   if ($conn->connect_error) {
     die("ERROR: No se puede conectar al servidor: " . $conn->connect_error);
